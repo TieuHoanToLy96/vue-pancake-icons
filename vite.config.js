a7099db46004e81c9906d6a79206cec1c2b136d6
TieuHoanToLy96 <tieuhoantoly@gmail.com>
2022-12-06 16:02:57 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     lib: {
       entry: path.resolve(__dirname, 'src/install.js'),
       name: 'VuePancakeIcons',
-      fileName: (format) => `vue-pancake-icons.${format}.js`,
+      fileName: (format) => `lib/vue-pancake-icons.${format}.js`,
     },
     target: 'es2015',
     rollupOptions: {
